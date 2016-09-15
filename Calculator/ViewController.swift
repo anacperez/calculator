a66b570d8c2762ba6171e71efc1276ef25b3994d
anaperez <anaperez@csumb.edu>
2016-09-14 19:37:45 -0700

    calculator part 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,56 +8,88 @@
 
 import UIKit
 import Darwin
-
 //definition of a class
+
 class ViewController: UIViewController {
     
-    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var display: UILabel! //user input label
+    @IBOutlet weak var history: UILabel! //history label
     
+    var userTypedDecimalNum = false
     var userIsIntheMiddleOfTyping = false
-    
-    var brain = CalculatorBrain()
+    var operandStack = Array<Double>()
+  
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsIntheMiddleOfTyping {
-            display.text = display.text! + digit
-            print("digit = \(digit)")
+            if(digit == "." && userTypedDecimalNum == false){
+                userTypedDecimalNum = true;
+                display.text = display.text! + digit
+                history.text! = history.text! + display.text! + digit
+            }
+            else if (digit != ".") {
+                display.text = display.text! + digit
+                history.text! = history.text! + display.text! + digit
+            }
         }
         else {
             display.text = digit
+            history.text! = history.text! + digit
             userIsIntheMiddleOfTyping = true
         }
         
     }
     @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
         if userIsIntheMiddleOfTyping {
+            history.text! = history.text! + operation
             enter()
         }
-        if let operation = sender.currentTitle {
-            if let result = brain.performOperation(operation) {
-                displayValue = result
-            } else{
-                displayValue = 0
-            }
+        switch operation{
+        case"×": performOperation {$0 * $1}
+        case"÷": performOperation {$1 / $0}
+        case"+": performOperation {$0 + $1}
+        case"−": performOperation {$0 - $1}
+        case"√": performOperation {sqrt($0)}
+        case"sin": performOperation {sin($0)}
+        case"cos": performOperation {cos($0)}
+        default: break
         }
     }
     
-    @IBAction func enter() {
+    //when user presses C
+    @IBAction func clear() {
         userIsIntheMiddleOfTyping = false
-        if let result = brain.pushOperand(displayValue) {
-            displayValue = result
-        } else{
-            displayValue  = 0
+        display.text! = "0"
+        history.text! = " "
+        operandStack.removeAll();
+    }
+    private func performOperation(operation: (Double, Double)->Double){
+        if operandStack.count >= 2{
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
         }
     }
-    var displayValue: Double {
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    @IBAction func enter() {
+        userIsIntheMiddleOfTyping = false
+        operandStack.append(displayValue)
+    }
+    
+    var displayValue: Double{
         get {
-            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+                return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
         }
         set{
             display.text = "\(newValue)"
-            userIsIntheMiddleOfTyping = false
+
         }
     }
 }
